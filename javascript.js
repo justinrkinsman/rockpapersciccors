f8c09c2b3927a25cc5b1a1fb4f065e303c64df75
Justin <justinrkinsman@gmail.com>
2022-04-25 19:24:41 -0300

    Finish basic ruleset of game

--- javascript.js
+++ javascript.js
@@ -5,13 +5,27 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection == 'Rock') {
-        return ("You Win");
-    } else if (playerSelection !== 'Rock') {
-        return ("You Lose");
+    if (playerSelection == computerSelection) {
+        return ('Tie Game');
+    } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
+        return (`You Win! ${playerSelection} beats ${computerSelection}`);
+    } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
+        return (`You Lose! ${computerSelection} beats ${playerSelection}`);
+    } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
+        return (`You Win! ${playerSelection} beats ${computerSelection}`);
+    } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
+        return (`You Lose! ${computerSelection} beats ${playerSelection}`);
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
+        return (`You Win! ${playerSelection} beats ${computerSelection}`);
+    } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
+        return (`You Lose! ${computerSelection} beats ${playerSelection}`);
     }
 }
 
-const playerSelection = 'Rock';
+function capitalizeFirstLetter(playerSelection) {
+    return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+}
+
+const playerSelection = 'Scissors';
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection))
