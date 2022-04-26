17757b4fb0370e825667f019e8ac1dc7dafe17eb
Justin <justinrkinsman@gmail.com>
2022-04-26 11:49:02 -0300

    Add playerWin, computerWin, and tieGame variables

--- javascript.js
+++ javascript.js
@@ -3,26 +3,27 @@ function computerPlay() {
     let randSel = selections[Math.floor(Math.random()*selections.length)];
     return randSel;
 }
+let playerWin = (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'SCISSORS') || (playerSelection.toUpperCase() == 'PAPER' && computerSelection.toUpperCase() == 'ROCK') || (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection.toUpperCase() == 'PAPER');
+let computerWin = (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'PAPER') || (playerSelection.toUpperCase() == 'PAPER' && computerSelection.toUpperCase() == 'SCISSORS') || (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection.toUpperCase() == 'ROCK');
+let tieGame = (playerSelection.toUpperCase() == computerSelection);
 
 function playRound(playerSelection, computerSelection) {
-    for (let i =1; i<=5; i++) {
         if (playerSelection.toUpperCase() == computerSelection) {
-            return ('Tie Game');
+            return('Tie Game');
         } else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'SCISSORS') {
-            return (`You Win! ${playerSelection.toUpperCase()} beats ${computerSelection}`);
+            return(`You Win! ${playerSelection.toUpperCase()} beats ${computerSelection}`);
         } else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'PAPER') {
-            return (`You Lose! ${computerSelection} beats ${playerSelection.toUpperCase()}`);
+            return(`You Lose! ${computerSelection} beats ${playerSelection.toUpperCase()}`);
         } else if (playerSelection.toUpperCase() == 'PAPER' && computerSelection == 'ROCK') {
-            return (`You Win! ${playerSelection.toUpperCase()} beats ${computerSelection}`);
+            return(`You Win! ${playerSelection.toUpperCase()} beats ${computerSelection}`);
         } else if (playerSelection.toUpperCase() == 'PAPER' && computerSelection == 'SCISSORS') {
-            return (`You Lose! ${computerSelection} beats ${playerSelection.toUpperCase()}`);
+            return(`You Lose! ${computerSelection} beats ${playerSelection.toUpperCase()}`);
         } else if (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection == 'PAPER') {
-            return (`You Win! ${playerSelection.toUpperCase()} beats ${computerSelection}`);
+            return(`You Win! ${playerSelection.toUpperCase()} beats ${computerSelection}`);
         } else if (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection == 'ROCK') {
-            return (`You Lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}`);
+            return(`You Lose! ${computerSelection} beats ${playerSelection.toUpperCase()}`);
         }
     }
-}
 
 let playerSelection = prompt('Make your selection:')
 const computerSelection = computerPlay();
