e38b6bf29bd991eea7d995d591b80b4dec55bfd9
Justin <justinrkinsman@gmail.com>
2022-04-27 14:58:24 -0300

    Complete game

--- javascript.js
+++ javascript.js
@@ -3,7 +3,8 @@ function computerPlay() {
     let randSel = selections[Math.floor(Math.random()*selections.length)];
     return randSel;
 }
-
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound() {
     let playerSelection = prompt('Make your Selection:')
@@ -13,59 +14,42 @@ function playRound() {
     let computerWin = (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'PAPER') || (playerSelection.toUpperCase() == 'PAPER' && computerSelection.toUpperCase() == 'SCISSORS') || (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection.toUpperCase() == 'ROCK');
     let tieGame = (playerSelection.toUpperCase() == computerSelection); {  
     if (playerWin) {
-            return (`You Win: ${playerSelection.toUpperCase()} beats ${computerSelection}`);
+            return (`You Win: ${playerSelection.toUpperCase()} beats ${computerSelection}. Player Score ${++playerScore}`);
         } else if (computerWin) {
-            return (`You Lose: ${computerSelection} beats ${playerSelection.toUpperCase()}`);
+            return (`You Lose: ${computerSelection} beats ${playerSelection.toUpperCase()}. Computer Score ${++computerScore}`);
         } else if (tieGame) {
             return ('Tie Game')
         }
     }
 }
 
-/*function winner() {
-    if (playerSelection.toUpperCase() == 'ROCK') {
-        if (computerSelection == 'SCISSORS') {
-            return (`You Win: ${playerSelection.toUpperCase()} beats ${computerSelection}`)
-        } else if (computerSelection == 'PAPER'){
-            return (`You Lose: ${computerSelection} beats ${playerSelection.toUpperCase}`)
-}
-    }
-}*/
-
-/*let winner = playRound();*/
-
-function game(playerWin, computerWin, tieGame) {
+function game() {
     for (let i = 1; i <= 5; i++) {
-        if (playRound(playerWin)) {
-            console.log(`Round ${i} ${playRound()}`);
-        } else if (playRound(computerWin)) {
-            console.log(`Round ${i} ${playRound}`);
-        } else if (playRound(tieGame)) {
-            console.log(`Round ${i} ${playRound}`);
-        } 
-      }
+        console.log(`Round ${i} ${playRound()}`);
+    }
+    if (computerScore < playerScore) {
+        console.log(`You win ${playerScore} - ${computerScore}`);
+    } else if (computerScore > playerScore) {
+        console.log(`You lose ${computerScore} - ${playerScore}`);
+    } else if (computerScore == playerScore) {
+        console.log('Tie game');
+    }
     console.log(`Game Over`)
 }
 
-console.log(game())
-
-
-
-
-
-
-/*let playerSelection = prompt('Make your selection:') //Player's selection is stored in a variable//
-let computerSelection = computerPlay() //Stores computer's selection in variable
-let playerWin = 
-
-function computerPlay() { //Determines computer's selection//
-    let selections = ['ROCK', 'PAPER', 'SCISSORS']
-    let randsel = selections[Math.floor(Math.random()*selections.length)];
-    return randsel;
+function scoreCount() {
+    for (let i = 0; i <= 5; i++)
+    console.log(`Player ${++playerScore}`)
+    console.log(`Computer ${++computerScore}`)
+    if (computerScore < playerScore) {
+        console.log(`You win ${playerScore} - ${computerScore}`);
+    } else if (computerScore > playerScore) {
+        console.log(`You lose ${computerscore} - ${playerScore}`);
+    } else if (computerScore == playerScore) {
+        console.log('Tie game');
+    } else {
+        console.log('Something went wrong...')
+    }
 }
 
-function playRound(playerSelection, computerSelection) {
-
-}
-
-console.log(playRound(playerSelection, computerSelection));*/
+game()
