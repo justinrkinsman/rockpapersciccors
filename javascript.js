9928b4dadea93cf5da432cbff5d950fd71563c62
Justin <justinrkinsman@gmail.com>
2022-05-01 15:24:51 -0300

    Change functionality of buttons

--- javascript.js
+++ javascript.js
@@ -6,53 +6,46 @@ function computerPlay() {
 let playerScore = 0;
 let computerScore = 0;
 
-let rock = document.createElement('button');
+/*let rock = document.createElement('button');
 rock.innerHTML = 'Rock';
 document.body.appendChild(rock);
-rock.addEventListener('click', function rockSelect(e){
-    console.log (e.innerText = 'ROCK')
-})
+rock.addEventListener('click', playRound());
 
 let paper = document.createElement('button');
 paper.innerHTML = 'Paper';
 document.body.appendChild(paper);
-paper.addEventListener('click', function paperSelect(e){
-    console.log (e.innerText = 'PAPER');
-})
+paper.addEventListener('click', playRound());
 
 let scissors = document.createElement('button');
 scissors.innerHTML = 'Scissors';
 document.body.appendChild(scissors);
-scissors.addEventListener('click', function scissorsSelect(e){
-    console.log (e.innerText = "SCISSORS");
-})
+scissors.addEventListener('click', playRound());*/
 
-function playerSelection() {
-    if (e.innerText == "ROCK") {
+/*function playerChoice() {
+    if (EventTarget.rock) {
         playerSelection == 'ROCK';
-    }else if (e.innerText == 'PAPER'){
+    }else if (EventTarget.paper){
         playerSelection == 'PAPER';
-    }else if (e.innerText == 'SCISSORS'){
-        playerSelection == 'SCISSORS'
+    }else if (EventTarget.scissors){
+        choiceScissors == 'SCISSORS'
     }
-};
-
+};*/
 
 function playRound() {
-    let playerSelection = playerSelection()
+    let playerSelection = prompt('Make your Selection');
     let computerSelection = computerPlay();
 
     let playerWin = (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'SCISSORS') || (playerSelection.toUpperCase() == 'PAPER' && computerSelection == 'ROCK') || (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection == 'PAPER');
     let computerWin = (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'PAPER') || (playerSelection.toUpperCase() == 'PAPER' && computerSelection == 'SCISSORS') || (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection == 'ROCK');
     let tieGame = (playerSelection.toUpperCase() == computerSelection); {  
     if (playerWin) {
-            console.log (`You Win: ${playerSelection.toUpperCase()} beats ${computerSelection}. Player Score ${++playerScore}`);
-        } else if (computerWin) {
-            console.log (`You Lose: ${computerSelection} beats ${playerSelection.toUpperCase()}. Computer Score ${++computerScore}`);
-        } else if (tieGame) {
-            console.log ('Tie Game')
-        }
+        console.log (`You Win: ${playerSelection.toUpperCase()} beats ${computerSelection}. Player Score ${++playerScore}`);
+    } else if (computerWin) {
+        console.log (`You Lose: ${computerSelection} beats ${playerSelection.toUpperCase()}. Computer Score ${++computerScore}`);
+    } else if (tieGame) {
+        console.log ('Tie Game')
     }
+}
 }
 
 function game() {
@@ -84,4 +77,4 @@ function scoreCount() {
     }
 }
 
-playRound();
+game();
